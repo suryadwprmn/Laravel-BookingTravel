a0c7f7e705fa291b5904aa960f57761beae08d7b
suryadwprmn <suryadwipramana18@gmail.com>
2025-02-04 15:06:54 +0700

    menambahkan custom requests

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden p-10 shadow-sm sm:rounded-lg"> 
-                <form method="POST" action=" " enctype="multipart/form-data">
+                <form method="POST" action="{{route('admin.categories.store')}}" enctype="multipart/form-data">
                     @csrf
 
                     <div>
